622a430826ef13995217882b1cb10490ba193f2f
Julian <jelaverallauro@gmail.com>
2024-05-27 08:45:47 -0300

    limpiando

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ import Alumno from "./src/models/alumno.js"
 import {sumar, restar, multiplicar, dividir} from "./src/modules/matematica.js"
 /*import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from
 "./modules/omdb-wrapper.js"*/
-import express from "express"; // hacer npm i express 
-import cors from "cors"; // hacer npm i cors
+import express from "express";
+import cors from "cors";
 
-// NO HACER OMDB, TERMINAR ALUMNO Y ENTREGAR
 
 const app = express();
 const port = 3000; // El puerto 3000 (http://localhost:3000)
@@ -106,11 +105,3 @@ app.delete('/alumnos', (req,res) => {
 app.listen(port, () => {
     console.log(`TP03 app listening on port ${port}`)
 })
-
-
-
-/* getIntegerOrDefault
-// ParseInt para validar numero sea si o si numero y que se haya mandado
-// con simbolo de pregunta se valida si es invalido o no
-// "node index.js" para iniciar en consola node
-*/
